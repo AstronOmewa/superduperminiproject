67bd7d3c2a6f679691d305883ed205990763fdfc
AstronOmewa <gladv242@gmail.com>
2026-01-17 02:05:55 +0300

    help me pls

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from hashlib import sha256
 import os
 
 # Создаем абсолютный путь к папке с шаблонами
@@ -14,9 +15,30 @@ def login():
     
     return render_template("login.html", current_user = current_user, )
 
-@app.route("/index")
+@app.route("/")
 def index():
-    return render_template("index.html")
+    current_user = {"is_authentificated" : True, "username" : "User0"}
+    
+    return render_template("index.html", current_user = current_user)
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        user = current_user = {"is_authentificated" : True, "username" : username, "email" : email, "password" : sha256(b'{password}').hexdigest() }
+        return redirect(url_for("index", current_user = current_user))
+    else:   
+        current_user = {"is_authentificated" : True, "username" : "User0"}
+        return render_template("register.html", current_user = current_user)
+
+
+for number in [404, 403]:
+    @ app.errorhandler(number)  
+    def page_not_found(e):  
+        return render_template('error.html', error_code = number), number  
 
 if __name__=="__main__":
     app.run()
