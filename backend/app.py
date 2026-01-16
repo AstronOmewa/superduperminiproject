f37393ecdb0c15eb7345adda1b4f1fcf92275d3b
AstronOmewa <gladv242@gmail.com>
2026-01-17 01:01:52 +0300

    Merged with main

--- backend/app.py
+++ backend/app.py
@@ -9,11 +9,13 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "..", "frontend", "templates")
 app = Flask(__name__, template_folder = TEMPLATE_DIR)
 
 @app.route("/login")
-def render_login():
-    return render_template("login.html")
+def login():
+    current_user = {"is_authentificated" : True, "username" : "User0"}
+    
+    return render_template("login.html", current_user = current_user, )
 
 @app.route("/index")
-def render_main():
+def index():
     return render_template("index.html")
 
 if __name__=="__main__":
