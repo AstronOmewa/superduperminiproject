d8e9ed398915d51192c847ba7fc8d2c9e742e033
AstronOmewa <gladv242@gmail.com>
2026-01-16 23:36:56 +0300

    informative commit message

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
+import os
 
-app = Flask(__name__)
+# Создаем абсолютный путь к папке с шаблонами
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "..", "frontend", "templates")
+
+app = Flask(__name__, template_folder = TEMPLATE_DIR)
+
+@app.route("/login")
+def render_login():
+    return render_template("login.html")
 
 @app.route("/index")
-def hello_world():
-    return render_template("templates/index.html")
+def render_main():
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run()
